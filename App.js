2adec323b3aa9340adfc73e2c1d69e1256b92a98
Samuel Osekeny <samuelpo49@icloud.com>
2025-05-08 10:42:22 -0600

    ep2-igniting app with parcel

--- App.js
+++ App.js
@@ -1,32 +1,15 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *         <h1>I am h1!</h1>
- *          <h2>I am h2!</h1>
- *       </div>
- * 
- * </div>
- * 
- * ReactElement(Object) => HTML(Browser Understants)
- * 
- * 
- */
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
-    [React.createElement(
-        "div",
-        {id: "child1"},
-        [React.createElement("h1", {}, "Iam a nested element of type h1 tag!"),
-        React.createElement("h2", {}, "Iam a nested element of type h2 tag!")]
-    ),
-    React.createElement(
-        "div",
-        {id: "child2"},
-        [React.createElement("h1", {}, "Iam a nested element of type h1 tag!"),
-        React.createElement("h2", {}, "Iam a nested element of type h2 tag!")]
-    )]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div",{id: "child1", key: "child1"},[
+        React.createElement("h1", {key: "c1-h1" }, "Iam a nested element of type h1 tag!"),
+        React.createElement("h2", {key: "c1-h2" }, "Iam a nested element of type h2 tag!"),
+    ]),
+    React.createElement("div",{id: "child2", key: "child2"},[
+        React.createElement("h1", {key: "c2-h1"}, "Iam a nested element of type h1 tag!"),
+        React.createElement("h2", {key: "c2-h2"}, "Iam a nested element of type h2 tag!"),
+    ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
