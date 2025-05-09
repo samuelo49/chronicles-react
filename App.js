95c1e5af0bfcf78d7d52aecde3cefcfa1b182ea3
Samuel Osekeny <samuelpo49@icloud.com>
2025-05-09 16:29:24 -0600

    ep-3 practing reactjs foundations

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div",{id: "child1", key: "child1"},[
-        React.createElement("h1", {key: "c1-h1" }, "Iam a nested element of type h1 tag!"),
-        React.createElement("h2", {key: "c1-h2" }, "Iam a nested element of type h2 tag!"),
-    ]),
-    React.createElement("div",{id: "child2", key: "child2"},[
-        React.createElement("h1", {key: "c2-h1"}, "Iam a nested element of type h1 tag!"),
-        React.createElement("h2", {key: "c2-h2"}, "Iam a nested element of type h2 tag!"),
-    ]),
-]);
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Steam Rolling with JSX🚀
+    </h1>
+);
+
+const HeadingComponent = () => (
+    <div id="container">
+        < Title />
+        <h1>Steam Rolling React Functional Components⛳️</h1>
+    </div>
+
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
